642b178db3a815808294164c455c28030f4beba7
Nicolas <nicolas.taltaud@edu.esiee.fr>
2017-11-14 18:43:46 +0100

    create all pages and links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {HomePage} from '../pages/home/home';
+import {MethodPage} from '../pages/method/method';
+import {HistoryPage} from '../pages/history/history';
 
 @Component({
   templateUrl: 'app.html'
@@ -14,15 +15,41 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any, icon: string, category: string, type: string }>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      {
+        title: 'Accueil',
+        component: HomePage,
+        icon: 'calculator',
+        category: 'general',
+        type: 'page'
+      },
+      {
+        title: 'Méthode',
+        component: MethodPage,
+        icon: 'help-circle',
+        category: 'general',
+        type: 'page'
+      },
+      {
+        title: 'Historique',
+        component: HistoryPage,
+        icon: 'time',
+        category: 'general',
+        type: 'page'
+      },
+      {
+        title: 'Langue',
+        component: null,
+        icon: 'flag',
+        category: 'other',
+        type: 'link'
+      },
     ];
 
   }
@@ -39,6 +66,16 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    switch (page.type) {
+      case 'link':
+        console.log('Change Language');
+        break;
+      case 'page':
+        this.nav.setRoot(page.component);
+        break;
+      default:
+        this.nav.setRoot(page.component);
+        break;
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 import { MethodPage } from '../pages/method/method';
+import { HistoryPage } from '../pages/history/history';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,8 +14,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
-    MethodPage
+    MethodPage,
+    HistoryPage,
   ],
   imports: [
     BrowserModule,
@@ -25,8 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
-    MethodPage
+    MethodPage,
+    HistoryPage,
   ],
   providers: [
     StatusBar,
