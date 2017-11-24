5cca7b3b3e35b1df4976f11ed7c58e70cfc026c2
Nicolas <nicolas.taltaud@edu.esiee.fr>
2017-11-24 15:42:33 +0100

    Add history basic layout + copy functions

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,12 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the HistoryPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HistoryPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, public translate: TranslateService) {
+    translate.getDefaultLang();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HistoryPage');
   }
 
+  presentToast(messageToShow: string) {
+    let toast = this.toastCtrl.create({
+      message: messageToShow,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  copyResults(){
+    this.translate.getDefaultLang();
+    this.translate.get('HISTORY_PAGE.COPY_RESULTS').subscribe(
+      value => {
+        let alertTitle = value;
+        this.presentToast(alertTitle);
+      }
+    )
+  }
 }
