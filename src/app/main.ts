4355c8dc165f0a3774733443fbef7d7d4383b166
Nicolas <nicolas.taltaud@edu.esiee.fr>
2017-11-30 22:09:24 +0100

    TODO for prod mode

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,7 +2,7 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app.module';
 
-//TODO: anebale prod mode 'ionic cordova build android --env=prod --release'
+//TODO: anebale prod mode 'ionic cordova build android/ios/windows --env=prod --release'
 import {enableProdMode} from "@angular/core";
 enableProdMode();
 
