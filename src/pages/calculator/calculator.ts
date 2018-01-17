3deb3c4e4918f7276d6be424c32cec8ace7bf6bb
Nicolas <nicolas.taltaud@edu.esiee.fr>
2018-01-17 18:09:23 +0100

    clean useless import

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {IonicPage, NavController} from 'ionic-angular';
 import {Validators, FormBuilder, FormGroup} from '@angular/forms';
 import {Storage} from '@ionic/storage';
 
